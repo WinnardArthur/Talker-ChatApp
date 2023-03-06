760b9e885e7459df2789209ab8079634608dbf57
Winnard Arthur <winnardarthur7@gmail.com>
2023-03-06 15:36:07 +0000

    initial setup for render hosting

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
